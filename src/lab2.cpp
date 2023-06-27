76167e27927d46e69a70e7827c3e813a6ae42383
Anusha <anushasaraf1306@gmail.com>
2023-06-27 11:02:12 -0500

    first commit

---
+++ src/lab2.cpp
@@ -0,0 +1,44 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/anusha/Desktop/labs/lab2/src/lab2.ino"
+void setup();
+void loop();
+#line 1 "/Users/anusha/Desktop/labs/lab2/src/lab2.ino"
+const int potentiometerPin = A0;  // Pin connected to the potentiometer
+const int ledPin = D7;            // Pin connected to the LED
+int voltageLevel;                 // Variable to store the voltage level
+
+
+
+
+/*
+ * Project lab2
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+   Serial.begin(9600);     // Initialize serial communication
+  pinMode(ledPin, OUTPUT); // Set the LED pin as an output
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  voltageLevel = analogRead(potentiometerPin);  // Read the voltage level from the potentiometer
+  float blinkSpeed = map(voltageLevel, 0, 4095, 100, 1000);  // Map the voltage range to the desired blink speed range
+
+  digitalWrite(ledPin, HIGH);   // Turn on the LED
+  delay(blinkSpeed);             // Wait for the specified blink speed
+
+  digitalWrite(ledPin, LOW);    // Turn off the LED
+  delay(blinkSpeed);             // Wait for the specified blink speed
+
+  Serial.println(voltageLevel);  // Print the voltage level to the Serial Monitor
+
+}
